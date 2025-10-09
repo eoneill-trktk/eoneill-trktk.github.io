cc8a4e1977049e2cef9f5805188abfb6af983c52
Eric ONeill <eoneill@traktekpartners.com>
2025-10-09 14:50:43 -0400

    c

--- about-timeline.js
+++ about-timeline.js
@@ -16,7 +16,7 @@ if (window.location.pathname.includes('/about/')) {
     styleTag.textContent = `
       .tl-carousel-container {
             position: relative;
-            max-width: 1440px;
+            max-width: 1080px;
             min-height: 800px;
             margin: 40px auto;
             padding: 0 15px;
