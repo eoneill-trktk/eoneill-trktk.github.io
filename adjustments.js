21037b81c57fd20922f1fbec3e5f0b4504e572e1
Eric ONeill <eoneill@traktekpartners.com>
2025-10-16 11:43:59 -0400

    c

--- adjustments.js
+++ adjustments.js
@@ -319,3 +319,26 @@ if (location.pathname.includes('/en-partners') || location.pathname.includes('ca
     }
   }, 100);
 }
+
+if (location.pathname.includes('career-fair-recruiter-registration-form')){
+  document.querySelectorAll('.Form__Element.FormChoice fieldset').forEach(fs => {
+  fs.querySelectorAll(':scope > span.choice-item').forEach(span => {
+    const input = span.querySelector('input');
+    let label = span.nextElementSibling?.matches('label.FormChoice__Label')
+      ? span.nextElementSibling
+      : (input ? fs.querySelector(`label[for="${input.id}"]`) : null);
+
+
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'inline-block';
+    wrapper.style.width = '100%';
+
+    fs.insertBefore(wrapper, span);    
+    wrapper.appendChild(span);          
+    if (label && label.parentNode === fs) {
+      wrapper.appendChild(label);       
+    }
+  });
+});
+
+}
